0464420ff657386f7325708c3157002f4b965c23
TiverT <jonasthebrinkmann@gmail.com>
2025-05-09 21:22:57 +0200

    Removed multiplication

--- index.js
+++ index.js
@@ -43,13 +43,12 @@ let isConnectedToGame = false;
 
 function createVehicle(id, uid, upgrades = [0, 0]) {
   const token = Buffer.alloc(180);
-
   token.uid = uid;
   //console.log(upgrades);
-  token.writeUInt32LE(upgrades[0], 0x23 * 4);
-  token.writeUInt16LE(id, 0x24 * 4);
-  token.writeUInt32LE(upgrades[1], 0x25 * 4);
-  token.writeUInt16BE(1, 0x26 * 4); //Page 26 is used for verification of somekind.
+  token.writeUInt32LE(upgrades[0], 0x8c); //0x23 * 4
+  token.writeUInt16LE(id, 0x90); //0x24 * 4
+  token.writeUInt32LE(upgrades[1], 0x94); //0x25 * 4
+  token.writeUInt16BE(1, 0x98); //0x26 * 4 //Page 26 is used for verification of somekind.
   return token;
 }
 
